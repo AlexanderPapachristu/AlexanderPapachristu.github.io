07de584de2e511296b351e4c4e31d9184ca79ae7
AlexanderPapachristu <apapachristu@gmail.com>
2025-03-10 14:08:45 -0400

    created arrays for images and alt text

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -5,8 +5,10 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageArray = ['images/pic1.jpg', 'images/pic2.jpg', 'images/pic3.jpg', 'images/pic4.jpg', 'images/pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const altArray = ['Closeup of a human eye', 'abstract painting', 'flowers', 'egyption drawings', 'large moth on a leaf'];
 
 /* Looping through images */
 
