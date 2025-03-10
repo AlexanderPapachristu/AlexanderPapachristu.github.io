a2334239eaa36f3a10d0065946489a3f5fbbb39e
AlexanderPapachristu <apapachristu@gmail.com>
2025-03-10 13:34:37 -0400

    added more js functionality to generate the story

--- lab4/part1/main.js
+++ lab4/part1/main.js
@@ -10,3 +10,28 @@ let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. Whe
 const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 const insertY = ["the soup kitchen", "Disneyland", "the White House"];
 const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+
+randomize.addEventListener('click', result);
+
+function result() {
+
+    let newStory = storyText;
+    let xItem = randomValueFromArray(insertX);
+    let yItem = randomValueFromArray(insertY);
+    let zItem = randomValueFromArray(insertZ);
+    if(customName.value !== '') {
+        const name = customName.value;
+
+    }
+
+    if(document.getElementById("uk").checked) {
+        const weight = Math.round(300);
+        const temperature =  Math.round(94);
+
+    }
+    newStory = newStory.replace(":insertx:", xItem);
+    newStory = newStory.replace(":inserty:", yItem);
+    newStory = newStory.replace(":insertz:", zItem);
+    story.textContent = ;
+    story.style.visibility = 'visible';
+}
